9530a158b00e8a1beffb5a99c27160b2b90c6291
Bruno Esteves <besteves@blissapplications.com>
2023-04-04 12:08:51 +0100

    Start project

---
+++ ApiBanco/User.Data/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User.Data.Models
+{
+    internal class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public DateTime DateTransaction { get; set; }
+        public string Attachment { get; set; }
+        public string fkUserID { get; set; }
+    }
+}
---
+++ ApiBanco/User.Data/Repository/UsersRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Data.Models;
+using User.Data.Interface;
+
+namespace User.Data.Repository
+{
+    public class UsersRepository : IUser
+    {
+
+        List<Users> lisUsers = new List<Users>
+        {
+            new Users{UserId="bruno@",Balance=0.0m,FirstName ="Bruno",LastName="Silva",Incomes=0.0m,Expense=.0m}
+        };
+
+
+        //Func to search user in the list "lisUsers"
+        public Users GetUserByID(string id)
+        {
+            return lisUsers.FirstOrDefault(x => x.UserId == id);
+        }
+
+        public Users UpdateUserByID(string id, decimal balance)
+        {
+
+            var user = GetUserByID(id);
+
+            user.Balance += balance;
+
+            return lisUsers.FirstOrDefault(x => x.UserId == id);
+
+        }
+
+    }
+}
